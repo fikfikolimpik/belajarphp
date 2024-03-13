65754e7f27d703f7200e79816729355ffb62f0d1
fikfikolimpik <taufiknurhdyt@gmail.com>
2024-03-13 14:49:27 +0700

    update

--- fungsi.php
+++ fungsi.php
@@ -1,9 +1,17 @@
 <?php
 
-
-function luadSegitiga($alas, $tinggi) {
+function luasSegitiga($alas, $tinggi) {
     $luas = 0.5 * $alas * $tinggi;
     return $luas;
 }
-echo luadSegitiga(2,4);
-?>
+
+function sum(...$input) {
+    $result = 0;
+    foreach ($input as $value) {
+        $result = $result + $value;
+    }
+
+    return $result;
+}
+
+echo sum(1,2,3,34,345,32,2);
